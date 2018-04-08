40a0fd46c800e76f5e65404cf29b391be83fe080
李振琦 <shouhutsh@gmail.com>
2018-04-08 21:45:18 +0800

    merge

--- app/model/article_model.go
+++ app/model/article_model.go
@@ -10,3 +10,16 @@ type ArticleShareRequest struct {
 type ArticleShareReturnData struct {
 	ShareTimes int32 `json:"shareTimes" binding:"required"`
 }
+
+type ArticleMarkRequest struct {
+	BaseRequest
+	OpenId     string `json:"openId"`
+	ArticleId  int64  `json:"articleId"`
+	DoMark     string `json:"doMark"`
+	CategoryId int64  `json:"categoryId"`
+}
+
+type ArticleMarkResponse struct {
+	BaseResponse
+	MarkTimes int64 `json:"markTimes"`
+}
