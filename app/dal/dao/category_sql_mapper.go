6125bd7d2df6bc3eb0834c02ae5ff6190f537d3d
Heng Li <liheeng@gmail.com>
2018-04-08 21:31:17 +0800

    Revmoed redundant return paramter names of function.

--- app/dal/dao/category_sql_mapper.go
+++ app/dal/dao/category_sql_mapper.go
@@ -7,7 +7,7 @@ import (
 
 var categorySqlMapper *CategorySqlMapper
 
-func GetCategorySqlMapper() (categorySqlMapper *CategorySqlMapper) {
+func GetCategorySqlMapper() (*CategorySqlMapper) {
 	if categorySqlMapper == nil {
 		categorySqlMapper = &CategorySqlMapper{}
 	}
@@ -19,11 +19,11 @@ type CategorySqlMapper struct {
 }
 
 // Call predefined sql template to insert category
-func (sqlMapper *CategorySqlMapper) AddCategory(category *entity.Category) (id int64, err error) {
+func (sqlMapper *CategorySqlMapper) AddCategory(category *entity.Category) (int64, error) {
 	return dal.DB.SqlTemplateClient("insert_category").InsertOne(category)
 }
 
 // 更新文章种类
-func (sqlMapper *CategorySqlMapper) UpdateCategory(category *entity.Category) (id int64 ,err error) {
+func (sqlMapper *CategorySqlMapper) UpdateCategory(category *entity.Category) (int64, error) {
 	return dal.DB.SqlTemplateClient("update_category").Update(category)
 }
--- app/dal/dao/wechat_user_sql_mapper.go
+++ app/dal/dao/wechat_user_sql_mapper.go
@@ -8,7 +8,7 @@ import (
 
 var wechatUserSqlMapper *WechatUserSqlMapper
 
-func GetWechatUserSqlMapper() (wechatUserSqlMapper *WechatUserSqlMapper) {
+func GetWechatUserSqlMapper() (*WechatUserSqlMapper) {
 	if wechatUserSqlMapper == nil {
 		wechatUserSqlMapper = &WechatUserSqlMapper{}
 	}
@@ -20,7 +20,7 @@ type WechatUserSqlMapper struct {
 }
 
 // Call predefined sql template to insert category
-func (sqlMapper *WechatUserSqlMapper) SelectUser(openId string) (user *entity.WechatUser, err error) {
+func (sqlMapper *WechatUserSqlMapper) SelectUser(openId string) (*entity.WechatUser, error) {
 	wechatUser := &entity.WechatUser{}
 	ok, err := dal.DB.SqlTemplateClient("select_user_by_openId").Get(wechatUser)
 	if err != nil {
@@ -36,12 +36,12 @@ func (sqlMapper *WechatUserSqlMapper) SelectUser(openId string) (user *entity.We
 }
 
 // Call predefined sql template to insert category
-func (wechatUserSqlMapper *WechatUserSqlMapper) UserRegister(wechatUser *entity.WechatUser) (bool bool, err error) {
+func (wechatUserSqlMapper *WechatUserSqlMapper) UserRegister(wechatUser *entity.WechatUser) (bool, error) {
 	paramMap := map[string]interface{}{"open_id": wechatUser.OpenId}
 	var user entity.WechatUser
 	return dal.DB.SqlTemplateClient("select_user_by_openId",paramMap).Get(&user)
 }
 
-func (wechatUserSqlMapper *WechatUserSqlMapper) InsertUser(wechatUser *entity.WechatUser) (id int64, err error) {
+func (wechatUserSqlMapper *WechatUserSqlMapper) InsertUser(wechatUser *entity.WechatUser) (int64, error) {
 	return dal.DB.SqlTemplateClient("insert_wechat_user").InsertOne(wechatUser)
 }
