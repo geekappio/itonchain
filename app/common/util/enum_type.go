1aa378beae66ba705d80c9b369710b2ec1d92329
Heng Li <liheeng@gmail.com>
2018-04-10 19:30:41 +0800

    Add enum support.

--- app/common/util/enum_type.go
+++ app/common/util/enum_type.go
@@ -1,20 +1,25 @@
 package util
 
+// Define an enumeration interface
 type Enum interface {
 	Value() string
 	ValueOf(value string) *EnumType
 }
 
+// Define an enumeration type
 type EnumType struct {
 	Value string
 }
 
+// Save all defined enumeration types.
 var enumTypeMap map[string]*EnumType = make(map[string]*EnumType)
 
+// Return enumeration type object by its value
 func (*EnumType) ValueOf(value string) *EnumType {
 	return enumTypeMap[value]
 }
 
+// Define a new enumeration type with specified value
 func DefEnumType(value string) (enumType *EnumType) {
 	enumType = &EnumType{Value: value}
 	enumTypeMap[value] = enumType
--- app/model/field_enum/field_enum.go
+++ app/model/field_enum/field_enum.go
@@ -2,18 +2,23 @@ package field_enum
 
 import "github.com/geekappio/itonchain/app/common/util"
 
+// 排序类型
 var ORDER_TYPE_NO_ORDER = util.DefEnumType("NO_ORDER")
 var ORDER_TYPE_TIME_DESC = util.DefEnumType("TIME_DESC")
 
+// YES and No
 var YES = util.DefEnumType("YES")
 var NO = util.DefEnumType("NO")
 
+// 文章状态
 var ARTICLE_STATE_OFFLINE = util.DefEnumType("OFFLINE")
 var ARTICLE_STATE_ONLINE = util.DefEnumType("ONLINE")
 var ARTICLE_STATE_EDIT = util.DefEnumType("DELETED")
 
+// 收藏和取消收藏
 var MARK = util.DefEnumType("MARK")
 var UNMARK = util.DefEnumType("UNMARK")
 
+// 关注和取消关注
 var FAVORITE = util.DefEnumType("FAVORITE")
 var UNFAVORITE = util.DefEnumType("UNFAVORITE")
