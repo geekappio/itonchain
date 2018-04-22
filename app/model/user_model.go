71f6d194ef709927664740c43873c07346723a84
fengkongling <fenglingtong66@126.com>
2018-04-22 17:46:50 +0800

    请求参数修改

--- app/model/user_model.go
+++ app/model/user_model.go
@@ -2,7 +2,7 @@ package model
 
 // Request of "/article_category/order/change" api
 type WechatUserRequest struct {
-	OpenId string `json:"user" binding:"required"`
+	OpenId string `json:"openId" binding:"required"`
 	NickName string `json:"nickName" binding:"required"`
 	AvatarlUrl string `json:"avatarlUrl"`
 	Gender string `json:"gender" binding:"required"`
