e1caf95977547c2dacd60c58f622dfece664fb7e
fengkongling <fenglingtong66@126.com>
2018-05-02 16:10:30 +0800

    完善查询文章代码提交

--- app/web/article_controller.go
+++ app/web/article_controller.go
@@ -163,12 +163,16 @@ func HandlerArticleList(request ArticleListRequest) (*ResponseModel) {
 	}
 
 	categoryId := request.SearchParams.CategoryId
-	categoryIdOther, err := strconv.ParseInt(categoryId, 10, 64)
-	if err != nil {
-		util.LogError("字符串转int64错误", categoryId, err)
-		return &ResponseModel{
-			ReturnCode: DB_ERROR.GetRespCode(),
-			ReturnMsg:  "查询用户信息失败",
+	var categoryIdOther int64
+	var errOther error
+	if categoryId != "" {
+		categoryIdOther, errOther = strconv.ParseInt(categoryId, 10, 64)
+		if errOther != nil {
+			util.LogError("字符串转int64错误", categoryId, errOther)
+			return &ResponseModel{
+				ReturnCode: ILLEGAL_PARAMETERS.GetRespCode(),
+				ReturnMsg:  "请求参数不合法",
+			}
 		}
 	}
 
